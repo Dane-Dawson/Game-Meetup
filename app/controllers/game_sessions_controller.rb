97a7dfa195b2e4d68acfdbae8a77a11d72816d46
github username <github email address>
2020-03-03 13:12:53 -0600

    merge

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -12,7 +12,9 @@ class GameSessionsController < ApplicationController
   end
 
   def create
-    @game_session = GameSession.new(game_session_params)
+    @game_session = GameSession.new(session_name: game_session_params[:session_name], game_id: game_session_params[:game_id], game_table_id: game_session_params[:game_table_id])
+    player = Player.find(game_session_params[:player_ids]) if game_session_params[:player_ids]
+    @game_session.players << player
     @game_session.save
     redirect_to game_session_path(@game_session)
   end
@@ -29,6 +31,6 @@ class GameSessionsController < ApplicationController
   private
 
   def game_session_params
-    params.require(:game_session).permit(:game_id, :game_table_id, :player_ids)
+    params.require(:game_session).permit(:game_id, :game_table_id, :player_ids, :session_name)
   end
 end
